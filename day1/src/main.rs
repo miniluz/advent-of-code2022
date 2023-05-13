c2b71ba45a30ef489313bc11f7b759bba42c503a
miniluz <javiermelon4fu@gmail.com>
2023-05-13 23:49:28 +0200

    map_while

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,33 +1,17 @@
 use itertools::Itertools;
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::cmp::Reverse;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let mut group_sums = include_str!("input.txt")
+    let answer = include_str!("input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
-        .batching(|it| {
-            let mut sum = None;
-            while let Some(Some(v)) = it.next() {
-                sum = Some(sum.unwrap_or(0) + v);
-            }
-            sum
-        })
-        .map(Reverse);
-
-    let mut heap = BinaryHeap::new();
-
-    for init in (&mut group_sums).take(3) {
-        heap.push(init);
-    }
-
-    for rest in group_sums {
-        heap.push(rest);
-        heap.pop();
-    }
-
-    let answer = heap.into_iter().map(|Reverse(v)| v).sum::<u64>();
+        .batching(|it| it.map_while(|x| x).sum1::<u64>())
+        .map(Reverse)
+        .k_smallest(3)
+        .map(|x| x.0)
+        .sum::<u64>();
 
     println!("{:?}", answer);
 

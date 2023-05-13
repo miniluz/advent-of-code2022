393ecddd1fe1ee2641a76f8d301d3be68c8cfea4
miniluz <javiermelon4fu@gmail.com>
2023-05-13 23:45:18 +0200

    heap solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,16 +1,35 @@
+use itertools::Itertools;
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let lines = include_str!("input.txt")
+    let mut group_sums = include_str!("input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
-        .collect::<Vec<_>>();
-    let elven_lead = lines
-        .split(|line| line.is_none())
-        .map(|group| group.iter().map(|v| v.unwrap()).sum::<u64>())
-        .max();
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .map(Reverse);
 
-    println!("{:?}", elven_lead);
+    let mut heap = BinaryHeap::new();
+
+    for init in (&mut group_sums).take(3) {
+        heap.push(init);
+    }
+
+    for rest in group_sums {
+        heap.push(rest);
+        heap.pop();
+    }
+
+    let answer = heap.into_iter().map(|Reverse(v)| v).sum::<u64>();
+
+    println!("{:?}", answer);
 
     Ok(())
 }

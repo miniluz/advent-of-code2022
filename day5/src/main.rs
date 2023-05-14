d136fe2901da4bf526422066c0c1d33d7c2c6bfd
miniluz <javiermelon4fu@gmail.com>
2023-05-14 20:25:21 +0200

    day 5

---
+++ day5/src/main.rs
@@ -0,0 +1,141 @@
+use std::fmt::{self};
+
+use itertools::Itertools;
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    character::complete::digit1,
+    combinator::{all_consuming, map, map_res},
+    multi::separated_list1,
+    sequence::{delimited, preceded, tuple},
+    Finish, IResult,
+};
+
+#[derive(Clone, Copy)]
+struct Crate(char);
+
+impl fmt::Debug for Crate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl fmt::Display for Crate {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+fn parse_crate(i: &str) -> IResult<&str, Crate> {
+    let first_char = |s: &str| Crate(s.chars().next().unwrap());
+    let f = delimited(tag("["), take(1_usize), tag("]"));
+    map(f, first_char)(i)
+}
+
+fn parse_space(i: &str) -> IResult<&str, ()> {
+    map(tag("   "), drop)(i)
+}
+
+fn parse_crate_or_hole(i: &str) -> IResult<&str, Option<Crate>> {
+    alt((map(parse_crate, Some), map(parse_space, |_| None)))(i)
+}
+
+fn parse_crate_line(i: &str) -> IResult<&str, Vec<Option<Crate>>> {
+    separated_list1(tag(" "), parse_crate_or_hole)(i)
+}
+
+#[derive(Debug)]
+struct Instruction {
+    quantity: usize,
+    src: usize,
+    dst: usize,
+}
+
+fn parse_number(i: &str) -> IResult<&str, usize> {
+    map_res(digit1, |s: &str| s.parse::<usize>())(i)
+}
+
+fn parse_pile_number(i: &str) -> IResult<&str, usize> {
+    map(parse_number, |i| i - 1)(i)
+}
+
+fn parse_instruction(i: &str) -> IResult<&str, Instruction> {
+    map(
+        tuple((
+            preceded(tag("move "), parse_number),
+            preceded(tag(" from "), parse_pile_number),
+            preceded(tag(" to "), parse_pile_number),
+        )),
+        |(quantity, src, dst)| Instruction { quantity, src, dst },
+    )(i)
+}
+
+struct Piles(Vec<Vec<Crate>>);
+
+impl fmt::Debug for Piles {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (i, pile) in self.0.iter().enumerate() {
+            writeln!(f, "Pile {}: {:?}", i, pile)?;
+        }
+        Ok(())
+    }
+}
+
+impl Piles {
+    fn apply(&mut self, ins: Instruction) {
+        for krate in (0..ins.quantity)
+            .map(|_| self.0[ins.src].pop().unwrap())
+            .collect::<Vec<_>>()
+            .iter()
+            .rev()
+        {
+            self.0[ins.dst].push(*krate);
+        }
+    }
+}
+
+fn transpose_rev<T>(v: Vec<Vec<Option<T>>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .rev()
+                .filter_map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
+
+fn main() {
+    let mut lines = include_str!("input.txt").lines();
+
+    let crate_lines: Vec<_> = (&mut lines)
+        .map_while(|line| {
+            all_consuming(parse_crate_line)(line)
+                .finish()
+                .ok()
+                .map(|(_, line)| line)
+        })
+        .collect();
+    let mut piles = Piles(transpose_rev(crate_lines));
+
+    println!("{piles:?}");
+
+    // we've consumed the "numbers line" but not the separating line
+    assert!(lines.next().unwrap().is_empty());
+
+    for instruction in lines.map(|line| all_consuming(parse_instruction)(line).finish().unwrap().1)
+    {
+        println!("{instruction:?}");
+        piles.apply(instruction);
+        println!("{piles:?}");
+    }
+
+    println!(
+        "answer: {}",
+        piles.0.iter().map(|pile| pile.last().unwrap()).join("")
+    );
+}
